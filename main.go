4d696c550b4d4b31c4842337ac4267d25ab34528
AsproSilon <103063584+AsproSilon@users.noreply.github.com>
2022-05-06 16:45:28 +0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// enter message and scan it , i think it is simple
+	var message string
+	fmt.Println("Enter the message")
+	fmt.Scan(&message)
+	//loop ti iterate
+	for i := 0; i < len(message); i++ {
+		c := message[i]
+		//condition : if your letter <x , everything is simple, but else you have to minus from the alphabet
+		if c >= 'a' && c <= 'z' {
+			c = c + 3
+			if c > 'z' {
+				c = c - 26
+
+			}
+		}
+		fmt.Printf("%c", c)
+	}
+}
